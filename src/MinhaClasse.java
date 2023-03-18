c5f23f4a607c1200e93f546c5f54ec04982efbd3
Diego de Araujo <101844511+Daraujocamara@users.noreply.github.com>
2023-03-17 21:01:12 -0300

    utilização de variaveis e parametros

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,7 +1,18 @@
 public class MinhaClasse {
     
   public static void main(String[] args) {
-    System.out.print ("olá mundo!");
 
-  }  
+    String primeiroNome = "Diego";
+    String segundoNome = "Araujo";
+
+    String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+    System.out.println(nomeCompleto);
+   
+  } 
+  
+  public static String nomeCompleto ( String primeiroNome, String segundoNome){
+    return "Resultado do metodo" + primeiroNome.concat(" ").concat(segundoNome);
+  }
+
 }
